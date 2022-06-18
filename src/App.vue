7671abb89a16c4e8f63c7bac979d41bac96cdefc
justFYM <manriquezfernando.ns@gmail.com>
2022-06-17 21:15:52 -0400

    Formato de las columnas, filas y Excalidraw listo, faltan los estilos

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,72 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed } from 'vue'
+const jugador = ref('X') //Variable que permite identificar al usuario que está en el turno.
+const tablero = ref([ //Tablero inicial, si se inicia el juego parte sin datos//
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', '']
+])
+const saberGanador = (tablero) => {
+  const jugadasGanadoras = [ //Aquí van las líneas que si se completan, se acaba el juego, en otras palabras, hay un ganador.
+    [1, 4, 7],
+    [6, 7, 8],
+    [0, 3, 6],
+    [2, 5, 8],
+    [0, 1, 2],
+    [3, 4, 5],
+    [0, 4, 8],
+    [2, 4, 6]]
+
+  for (let i = 0; i < jugadasGanadoras.length; i++) {
+    const [a, b, c] = jugadasGanadoras[i]
+    if (tablero[a] && tablero[a] === tablero[b] && tablero[a] === tablero[c]) { //
+      return tablero[a]
+    }
+  }
+  return null
+}
+const ganador = computed(() => saberGanador(tablero.value.flat()))
+const jugada = (x, y) => {
+	if (ganador.value) return
+	if (tablero.value[x][y]) return
+	tablero.value[x][y] = jugador.value
+	jugador.value = jugador.value === 'X' ? 'O' : 'X' //Operador ternario que cambia de turno al jugador que ya hizo su jugada.
+}
+const reiniciarElJuego = () => {
+	tablero.value = [   /* Éste es el tablero pincipal, el que aparecerá sin datos cuando se visualice la aplicación web*/
+		['', '', ''],
+		['', '', ''],
+		['', '', '']
+	]
+	jugador.value = 'X'  //Se le asigna la referencia X al usuario para detectar su jugada una vez que la haga.
+}
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+	<main class="text-center">
+		<h1>Taller 2</h1>
+		<div class="flex flex-col items-center mb-8">
+			<div 
+				v-for="(row, x) in tablero" 
+				:key="x"
+				class="flex">
+				<div 
+					v-for="(cell, y) in row" 
+					:key="y" 
+					@click="jugada(x, y)" 
+					:class="`border-2 border-blue  w-40 h-40 flex items-center justify-center material-icons-outlined text-5xl cursor-pointer ${cell === 'X' ? 'text-yellow-500' : 'text-red-900'}`">
+					{{ cell === 'X' ? 'X' : cell === 'O' ? '0' : '' }} <!--Ternario que define las marcas de cada jugador, y deja vacío el tablero en caso de que no hayan jugadas-->
+				</div>
+			</div>
+		</div>
+
+		<div class="text-center">
+				</div>
+	</main>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+	@apply bg-cyan-900 text-white;
 }
 </style>
