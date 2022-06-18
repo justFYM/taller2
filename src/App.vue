e7e0bcb1a09eea4f11efae2105385ee101cf0a20
justFYM <manriquezfernando.ns@gmail.com>
2022-06-17 21:27:07 -0400

    Botón Reiniciar con los estilos de Tailwind css implementado

--- src/App.vue
+++ src/App.vue
@@ -61,6 +61,8 @@ const reiniciarElJuego = () => {
 		</div>
 
 		<div class="text-center">
+      <button @click="reiniciarElJuego" class="px-6 py-4 bg-violet-500 rounded font-bold hover:bg-violet-800 duration-300">Reiniciar</button>
+	
 				</div>
 	</main>
 </template>
